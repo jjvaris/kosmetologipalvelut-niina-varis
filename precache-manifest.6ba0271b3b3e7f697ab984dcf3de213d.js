3cd4e963bc9ff168cb91025802c5c3ddfa63d535
Jouni Varis <jjvaris@gmail.com>
2020-06-27 21:20:02 +0300

    Updates

---
+++ precache-manifest.6ba0271b3b3e7f697ab984dcf3de213d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d905a20ad9f434a0e42e4439a645132",
+    "url": "/index.html"
+  },
+  {
+    "revision": "877a6861f53e4108f39e",
+    "url": "/static/css/main.a6668299.chunk.css"
+  },
+  {
+    "revision": "52764ba0576773785c35",
+    "url": "/static/js/2.52c43697.chunk.js"
+  },
+  {
+    "revision": "877a6861f53e4108f39e",
+    "url": "/static/js/main.03ac6f41.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "958dce7ea079a444af655d997a10a101",
+    "url": "/static/media/brushes.958dce7e.jpg"
+  },
+  {
+    "revision": "febde53b3c0c8759cf0beefaa4cf93b0",
+    "url": "/static/media/jane_iredale.febde53b.jpg"
+  },
+  {
+    "revision": "a1b2e72f56f9db341727d58dc5c373ac",
+    "url": "/static/media/landing.a1b2e72f.jpg"
+  },
+  {
+    "revision": "8fd4db089e7a2182d1f4f6827ee9ecb2",
+    "url": "/static/media/logo2.8fd4db08.jpg"
+  },
+  {
+    "revision": "a207ef2be9d23a5047cae8b411c5ff1b",
+    "url": "/static/media/pikkujoulut.a207ef2b.jpg"
+  },
+  {
+    "revision": "cab7f1f2997223d3d51bec7a761bfd19",
+    "url": "/static/media/products.cab7f1f2.jpg"
+  },
+  {
+    "revision": "b29151b8c7c8ad8762222fbc40f79491",
+    "url": "/static/media/tietosuojaseloste.b29151b8.pdf"
+  }
+]);
