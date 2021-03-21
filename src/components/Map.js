e644461e552d9a339b6f3cc749e08c9d1efc0b89
Jouni Varis <jjvaris@gmail.com>
2021-03-21 22:08:40 +0200

    fix console errors

--- src/components/Map.js
+++ src/components/Map.js
@@ -6,11 +6,11 @@ const Map = () => (
     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1836.7880760663613!2d29.781739215801466!3d62.58908865252848!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x469b8730fe02c6d5%3A0xe45b22da12aafe95!2sKosmetologipalvelut%20Niina%20Varis!5e0!3m2!1sfi!2sfi!4v1606664183511!5m2!1sfi!2sfi"
     width="100%"
     height="300"
-    frameborder="0"
+    frameBorder="0"
     style={{ border: 0 }}
-    allowfullscreen=""
+    allowFullScreen=""
     aria-hidden="false"
-    tabindex="0"
+    tabIndex="0"
     title="map"
   ></iframe>
 );
