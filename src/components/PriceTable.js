2b17e0cf5bfb0714101493e079756617b6b683b3
Jouni Varis <jouni.varis@gofore.com>
2023-06-27 17:21:52 +0300

    Update PriceTable.js

--- src/components/PriceTable.js
+++ src/components/PriceTable.js
@@ -51,7 +51,7 @@ const PriceTable = () => (
         <td className="price-table__service price-table__service--special">
           Käsivarret
         </td>
-        <td className="price-table__price">25 €</td>
+        <td className="price-table__price">30 €</td>
       </tr>
       <tr className="price-table__row">
         <td className="price-table__service price-table__service--special">
@@ -75,7 +75,7 @@ const PriceTable = () => (
         <td className="price-table__service price-table__service--special">
           Kasvot
         </td>
-        <td className="price-table__price">20 €</td>
+        <td className="price-table__price">25 €</td>
       </tr>
       <tr className="price-table__row">
         <td className="price-table__service price-table__service--special">
