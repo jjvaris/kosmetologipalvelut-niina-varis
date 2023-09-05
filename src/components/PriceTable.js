caa5860a9fc39f84d53a4e456269a3d9fb782eb0
Jouni Varis <jjvaris@gmail.com>
2023-09-05 10:38:08 +0300

    Update PriceTable.js

--- src/components/PriceTable.js
+++ src/components/PriceTable.js
@@ -33,7 +33,7 @@ const PriceTable = () => (
         <td className="price-table__service price-table__service--special">
           Bikinilinja
         </td>
-        <td className="price-table__price">20 €</td>
+        <td className="price-table__price">25 €</td>
       </tr>
       <tr className="price-table__row">
         <td className="price-table__service price-table__service--special">
